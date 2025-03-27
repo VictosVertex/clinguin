7458c4632bac12c6683b103a2a4e4f6ff25d8a05
VictosVertex <Victos.Vertex@gmail.com>
2025-03-27 12:39:14 +0100

    add everything in hopes it works for the presentation

--- angular_frontend/src/app/component-creation.service.ts
+++ angular_frontend/src/app/component-creation.service.ts
@@ -1,27 +1,34 @@
-import { Component, ComponentRef, Injectable, Type, ViewContainerRef } from '@angular/core';
-import { ContainerComponent } from './container/container.component';
-import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
-import { LabelComponent } from './label/label.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
-import { ButtonComponent } from './button/button.component';
-import { CanvasComponent } from './canvas/canvas.component';
-import { TextfieldComponent } from './textfield/textfield.component';
-import { ModalComponent } from './modal/modal.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { CheckboxComponent } from './checkbox/checkbox.component';
-import { CollapseComponent } from './collapse/collapse.component';
+import {
+  Component,
+  ComponentRef,
+  Injectable,
+  Type,
+  ViewContainerRef,
+} from "@angular/core";
+import { ContainerComponent } from "./container/container.component";
+import { DropdownMenuComponent } from "./dropdown-menu/dropdown-menu.component";
+import { LabelComponent } from "./label/label.component";
+import { MenuBarComponent } from "./menu-bar/menu-bar.component";
+import { ButtonComponent } from "./button/button.component";
+import { CanvasComponent } from "./canvas/canvas.component";
+import { TextfieldComponent } from "./textfield/textfield.component";
+import { ModalComponent } from "./modal/modal.component";
+import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
+import { CheckboxComponent } from "./checkbox/checkbox.component";
+import { CollapseComponent } from "./collapse/collapse.component";
+import { RangeSliderComponent } from "./range-slider/range-slider.component";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ComponentCreationService {
+  constructor() {}
 
-  constructor() { }
-
-
-  componentCreation(child: ViewContainerRef, key: string): ComponentRef<any> | null {
-
-    let dict: { key: string, value: Type<any> }[] = [
+  componentCreation(
+    child: ViewContainerRef,
+    key: string
+  ): ComponentRef<any> | null {
+    let dict: { key: string; value: Type<any> }[] = [
       { key: "container", value: ContainerComponent },
       { key: "dropdown_menu", value: DropdownMenuComponent },
       { key: "label", value: LabelComponent },
@@ -31,24 +38,29 @@ export class ComponentCreationService {
       { key: "modal", value: ModalComponent },
       { key: "progress_bar", value: ProgressBarComponent },
       { key: "checkbox", value: CheckboxComponent },
-	  { key: "collapse", value: CollapseComponent }
+      { key: "collapse", value: CollapseComponent },
+      { key: "range", value: RangeSliderComponent },
+    ];
 
-    ]
+    let component = null;
 
-    let component = null
-
-    let index = dict.findIndex(item => item.key == key)
+    let index = dict.findIndex((item) => item.key == key);
     if (index >= 0) {
-      component = child.createComponent(dict[index].value)
+      component = child.createComponent(dict[index].value);
     }
 
-    if (component == null && key != "menu_bar" && key != "context_menu" && key != "message") {
-      console.log("Could not associate component key with an component: " + key)
+    if (
+      component == null &&
+      key != "menu_bar" &&
+      key != "context_menu" &&
+      key != "message"
+    ) {
+      console.log(
+        "Could not associate component key with an component: " + key
+      );
       //throw new Error("Could not associate component key with an component: " + key)
     }
 
-
-    return component
+    return component;
   }
 }
-
